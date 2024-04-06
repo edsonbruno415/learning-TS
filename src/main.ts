2951b894af46e10fc4f1b1abd03c55a55258fa07
Edson Bruno <edsonbruno415@gmail.com>
2024-04-06 12:23:16 -0300

    starting with ts-node development

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import Todo from "./Todo";
 
-const todo = new Todo('Hello World');
+const todo = new Todo('Estudar TypeScript');
+const todo1 = new Todo('Estudar Clean Architecture');
+const todo2 = new Todo('Estudar Clean Code');
 
-console.log('TODO:', todo.description);
+console.log('TODO 0:', todo.description);
+console.log('TODO 1:', todo1.description);
+console.log('TODO 2:', todo2.description);
