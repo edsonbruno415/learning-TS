0b1caefb581c1bb8b8e291f04a3aeeee3222682d
Edson Bruno <edsonbruno415@gmail.com>
2024-04-06 11:58:49 -0300

    starting with ts

---
+++ src/Todo.ts
@@ -0,0 +1,5 @@
+export default class Todo {
+  constructor(readonly description: string){
+
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import Todo from "./Todo";
+
+const todo = new Todo('Hello World');
+
+console.log('TODO:', todo.description);
